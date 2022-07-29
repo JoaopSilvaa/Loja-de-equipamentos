d42a6c75ffe7905432b4ad5b0630294aff47db82
Joao Antonio <joaopsilva.dev@gmail.com>
2022-07-29 18:01:24 -0300

    feat: :construction: requisito 6 completo

---
+++ src/middlewares/product.middleware.ts
@@ -0,0 +1,56 @@
+import { NextFunction, Request, Response } from 'express';
+import Product from '../interfaces/product.interface';
+
+const properties = ['name', 'amount'];
+
+function validateProperties(product: Product): [boolean, string | null] {
+  for (let i = 0; i < properties.length; i += 1) {
+    if (!Object.prototype.hasOwnProperty.call(product, properties[i])) {
+      return [false, properties[i]];
+    }
+  }
+  return [true, null];
+}
+
+function validateValues(product: Product): [boolean, string | null] {
+  const entries = Object.entries(product);
+  for (let i = 0; i < properties.length; i += 1) {
+    const [property, value] = entries[i];
+    if (typeof value !== 'string') {
+      return [false, property];
+    }
+  }
+  return [true, null];
+}
+
+function validateLength(product: Product): [boolean, string | null] {
+  const entries = Object.entries(product);
+  for (let i = 0; i < properties.length; i += 1) {
+    const [property, value] = entries[i];
+    if (value.length < 2) {
+      return [false, property];
+    }
+  }
+  return [true, null];
+}
+
+function validationProduct(req: Request, res: Response, next: NextFunction) {
+  const product: Product = req.body;
+  let [valid, property] = validateProperties(product);
+  if (!valid) {
+    return res.status(400).json({ message: `"${property}" is required` });
+  }
+  [valid, property] = validateValues(product);
+  if (!valid) {
+    return res.status(422).json({ message: `"${property}" must be a string` });
+  }
+  [valid, property] = validateLength(product);
+  if (!valid) {
+    return res.status(422).json({
+      message: `"${property}" length must be at least 3 characters long` });
+  }
+
+  next();
+}
+  
+export default validationProduct;
--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import ProductController from '../controllers/product.controller';
+import validationProduct from '../middlewares/product.middleware';
 
 const router = Router();
 
 const product = new ProductController();
 
-router.post('/products', product.create);
+router.post('/products', validationProduct, product.create);
 router.get('/products', product.getAll);
 
 export default router;
